276dfabacecebdc45abd81900c15cb543d495a46
Michael Ballantyne <michael.ballantyne@gmail.com>
2013-02-16 18:50:34 -0700

    Even more layout!

--- output/static/gsl.js
+++ output/static/gsl.js
@@ -90,66 +90,100 @@ var debounce = function (threshold, func) {
 }
 
 var size = function() {
-    screenh = $(window).height();
-    screenw = $(window).width();
-    twocol = screenw >= 1000;
+    var screenh = $(window).height();
+    var screenw = $(window).width();
 
-    graph = $('#graph');
-    graphbox = $('#levelbox');
+    // We'll use a two column layout if the horizontal screen resoultion is >= 1000.
+    var twocol = screenw >= 1000;
 
-    gaddh = screenh - (graphbox.offset().top + graphbox.outerHeight())
-
+    // Apply or remove classes that create the two column layout.
     if (twocol) {
-        leftover = 20;
+        $('#levelcolumn').addClass('twocol')
+        $('#weathercolumn').addClass('twocol')
     } else {
-        leftover = 80;
+        $('#levelcolumn').removeClass('twocol')
+        $('#weathercolumn').removeClass('twocol')
     }
 
-    gnewh = graph.height() + gaddh - leftover;
+    var graph = $('#graph');
+    var graphbox = $('#levelbox');
 
-    minGraphHeight = 300;
+    var gaddh = screenh - (graphbox.offset().top + graphbox.outerHeight())
+
+    // If in the two column layout, ideally we'd like the graph to end nearly at the
+    // bottom of the screen, with just some padding. In the one column layout, we
+    // want to be sure the use knows there is more to the page to scroll to.
+    if (twocol) {
+        var leftover = 20;
+    } else {
+        var leftover = 80;
+    }
+
+    var gnewh = graph.height() + gaddh - leftover;
+
+    // If the screen hight is small enough, the graph could be too small. Make sure it
+    // is at least 300px high, even if that means the user must scroll.
+    var minGraphHeight = 300;
     if (gnewh < minGraphHeight) {
         gnewh = minGraphHeight;
     }
 
     graph.height(gnewh);
 
+    var radarbox = $('#radarbox')
+    var radarimg = $('#radarbox img')
+    
+    radarbox.css('marginTop', 0);
+
+    // In two column layout, push radar image down so that the bottom of it 
+    // matches with the bottom of the graph
     if (twocol) {
-        setTimeout(function() {
-        radarbox = $('#radarbox')
-        radarimg = $('#radarbox img')
-        
-        radarbox.css('marginTop', 0);
-        
-        bottom = screenh - leftover;
-        raddh = bottom - (radarbox.offset().top + radarbox.outerHeight())
+        var bottom = screenh - leftover;
+        var raddh = bottom - (radarbox.offset().top + radarbox.outerHeight())
 
         if (raddh > 0) {
             radarbox.css('marginTop', raddh);
         }
-        }, 50);
     }
 }
 
 
 var createGraph = function() {
+    // Remove graph if we've drawn it before.
     $('#graph').html('');
+
     new Ico.LineGraph($('#graph')[0], {saline: lakedata.saline, saltair: lakedata.saltair}, {
         stroke_width: "1",
+        width: $('#graph').width(),
+        height: $('#graph').height(),
         curve_amount: 0,
+        // Ico should figure this out from the div it is inside, but that is broken on IE
+        background_colour: "#FFFFFF",
         labels: lakedata.labels,
         colours: {
-            saline: "#AD1F00",
-            saltair: "#2a9338"
+            saline: "#AD1F00", // north, red
+            saltair: "#2a9338" // south, green
         }
     });
+
+    // The graph was oddly positioned in IE8 without this.
+    $('#graph div').css('overflow', 'visible')
 }
 
+// We need the radar image to be loaded so that its dimensions are known
+// to the layout engine before we try and lay the page out, so wait
+// till onLoad rather than use jQuery's DOM ready.
 $(window).load(function() {
     size();
+
+    // The content starts out hidden so that the user doesn't see it draw
+    // and then be moved. Must set visible before we draw graph or it won't appear in IE8.
+    $('#contentArea').css('visibility', 'visible');
+    
     createGraph();
 
-    $(window).resize(debounce(250, function() {
+    // Resize elements and redraw after window resize, but only after they've stopped dragging.
+    $(window).resize(debounce(500, function() {
         size();
         createGraph();
     }));
