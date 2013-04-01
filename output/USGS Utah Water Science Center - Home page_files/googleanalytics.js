915d3a2683cee6f089df09b3f44cd48dab9c0c82
Michael Ballantyne <michael.ballantyne@gmail.com>
2013-04-01 11:20:47 -0600

    fit into usgs template

---
+++ output/USGS Utah Water Science Center - Home page_files/googleanalytics.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+var _gaq = _gaq || [];
+_gaq.push(
+['_setAccount', 'UA-23479674-1'],
+['_setDomainName', '.usgs.gov'],
+['_setAllowLinker', true],
+['_setMaxCustomVariables', 10],
+['_gat._anonymizeIp'],
+['_setVisitorCookieTimeout', 15768000000],
+['_trackOutbound',{onBounce:true, action:'click'}],
+['_trackDownload',{onBounce:true, method:'event', action:'click'}],
+['_trackMailTo',{onBounce:true, action:'click'}],
+['_trackRealBounce'],
+['_trackSocial'],
+['_trackError'],
+['_trackLoadTime'],
+['_trackPageview']
+); 
+
+var addthis_config = {
+  data_ga_property: 'UA-23479674-1'
+  }; 
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
